a0b80468f7a40be6f30abdf75068716112eda3b1
Matej Sajgal <roarbb@gmail.com>
2020-04-09 21:22:11 +1000

    Replaces individual button streams with one stream

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { useState } from 'react';
 import { fromEvent } from 'rxjs';
-import { tap, map } from 'rxjs/operators'
+import { tap, map, filter } from 'rxjs/operators'
 import styled from 'styled-components';
 
 const LOCAL_STORAGE_COUNT = 'count';
@@ -19,12 +19,12 @@ const Count = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${({count}) => {
-    if(count < 1) {
+  color: ${({ count }) => {
+    if (count < 1) {
       return 'var(--color-red)';
     }
 
-    if(count > 99) {
+    if (count > 99) {
       return 'var(--color-green)';
     }
   }};
@@ -71,38 +71,43 @@ const Button = styled.button`
 
 const Counter = () => {
   const [count, setCount] = useState(parseInt(localStorage.getItem(LOCAL_STORAGE_COUNT)) || 0);
+  const buttonsWrapperRef = useRef();
 
   const buttons = [
-    { label: '+10', ref: useRef(), value: 10 },
-    { label: '+5', ref: useRef(), value: 5 },
-    { label: '+1', ref: useRef(), value: 1 },
-    { label: '-10', ref: useRef(), value: -10 },
-    { label: '-5', ref: useRef(), value: -5 },
-    { label: '-1', ref: useRef(), value: -1 },
-    { label: 'reset', ref: useRef(), value: 0 },
+    { label: '+10', value: 10 },
+    { label: '+5', value: 5 },
+    { label: '+1', value: 1 },
+    { label: '-10', value: -10 },
+    { label: '-5', value: -5 },
+    { label: '-1', value: -1 },
+    { label: 'reset', value: 0 },
   ];
 
   useEffect(() => {
-    const buttonsStreams = buttons.map(({ref, value}) => {
-      return fromEvent(ref.current, 'click')
-        .pipe(
-          map(() => value === 0 ? 0 : count + value),
-          tap(setCount),
-          tap(newCountValue => localStorage.setItem(LOCAL_STORAGE_COUNT, newCountValue)),
-        )
-        .subscribe();
-    });
+    const click$ = fromEvent(buttonsWrapperRef.current, 'click')
+      .pipe(
+        filter((event) => event.srcElement.dataset.value),
+        map(event => parseInt(event.srcElement.dataset.value)),
+        map((value) => value === 0 ? 0 : count + value),
+        tap(setCount),
+        tap(newCountValue => localStorage.setItem(LOCAL_STORAGE_COUNT, newCountValue)),
+      )
+      .subscribe();
 
-    return () => {
-      buttonsStreams.forEach(button$ => button$.unsubscribe())
-    };
-  }, [count, buttons]);
+    return () => click$.unsubscribe();
+  }, [count, buttonsWrapperRef]);
 
   return (
     <Wrapper>
       <Count count={count}>{count}</Count>
-      <ButtonsWrapper>
-        {buttons.map((button) => <Button ref={button.ref} key={button.label}>{button.label}</Button>)}
+      <ButtonsWrapper ref={buttonsWrapperRef}>
+        {buttons.map((button) => <Button
+          ref={button.ref}
+          key={button.label}
+          data-value={button.value}
+        >
+          {button.label}
+        </Button>)}
       </ButtonsWrapper>
     </Wrapper>
   );
